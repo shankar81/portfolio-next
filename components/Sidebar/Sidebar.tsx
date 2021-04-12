93f26e5562189c586f1245bc9dd6ca3f9e363e55
shankar81 <shankarsawant81@gmail.com>
2021-04-12 20:31:26 +0530

    added circular texts

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 import classes from "./Sidebar.module.css";
 
+const skills = [
+  "HTML5",
+  "CSS3",
+  "Javascript",
+  "React JS",
+  "React Native",
+  "Kotlin",
+  "Java",
+];
 const Sidebar = () => {
   return (
     <div className={classes.container}>
@@ -25,6 +34,26 @@ const Sidebar = () => {
             width={1000}
             className={classes.image}
           />
+        </div>
+
+        <div className={classes.skills}>
+          {skills.map((el, index) => {
+            let center = Math.round(skills.length / 2) - 1;
+            let margin = index - center;
+
+            if (center !== index && margin < 0) {
+              margin = -margin;
+            }
+            return (
+              <h4
+                key={index}
+                style={{ marginLeft: -(margin * 40) }}
+                className={classes.skill}
+              >
+                {el}
+              </h4>
+            );
+          })}
         </div>
       </div>
     </div>
